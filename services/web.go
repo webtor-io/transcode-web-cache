9a33bd2947a4f07f86de6302f15332ea488000db
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2021-03-13 13:43:49 +0300

    add logging

--- services/web.go
+++ services/web.go
@@ -83,6 +83,7 @@ func (s *Web) Serve() error {
 			return
 		}
 		if c == nil {
+			log.Warnf("Content not found path=%v hash=%v key=%v", r.Header.Get("X-Info-Hash"), r.Header.Get("X-Origin-Path"), key)
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
